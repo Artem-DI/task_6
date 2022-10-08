174ac58a5abee08bfd78306f5445d3d892c740b5
Artem-DI <111899484+Artem-DI@users.noreply.github.com>
2022-10-09 01:49:48 +0300

    Update task_6_part_3.py

--- task_6_part_3.py
+++ task_6_part_3.py
@@ -50,28 +50,3 @@ for t in threads:
 
 end = time.time()
 print(f'Время выполнение параллельного запуска: {end - start}')
-
-
-'''import requests
-import time
-from threading import Thread
-
-def get_html(link):
-	text = requests.get(link).text
-	print(f'{link} text length is {len(text)}')
-
-links=['https://pythonru.com/', 'https://yandex.ru/', 'https://github.com/xrus92', 'https://brunoyam.com/', 'https://www.google.com/']
-threads = [Thread(target = get_html, args = (links[i], )) for i in range(5)]
-time1 = time.time() 
-for t in threads:
-	t.start()
-	t.join()
-print(f'Время последовательного выполнения: {time.time()-time1}\n')
-
-threads = [Thread(target = get_html, args = (links[i], )) for i in range(5)]
-time1 = time.time()
-for t in threads:
-	t.start()
-for t in threads:
-	t.join()
-print(f'Время параллельного выполнения: {time.time()-time1}')'''
